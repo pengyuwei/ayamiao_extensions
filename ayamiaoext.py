0cc7a5b97861418b29df598ab829a8f4bdab150f
Peng Yuwei <pengyuwei@gmail.com>
2018-10-12 16:33:12 +0800

    Update ayamiaoext.py

--- ayamiaoext.py
+++ ayamiaoext.py
@@ -6,11 +6,6 @@ def init(cmd_mapping):
     return cmd_mapping
 
 # raw:对话的原始数据
-#{
-#  'ActualNickName': "昵称",
-#  'Content': "聊天内容",
-#  'FromUserName': "用户名"
-#}
 # msg:对话的文本内容
 def on_extcmd(raw, msg):
     return
